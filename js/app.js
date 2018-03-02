1d7fb5a3cfaf03701557ec0f6b288a8fdbedf1f6
Ryan Groesch <ryan.groesch@gmail.com>
2018-03-01 17:12:52 -0800

    Cara and ryan final changes to CSS and fixed learn page issues

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 var base = 0;
 var randoResults = ['skyBurial', 'vikingBurial', 'tradBurial', 'scienceBurial', 'cremation', 'spaceBurial', 'seaBurial'];
 var elementArray = ['#skyburial', '#viking', '#burial', '#science', '#cremation', '#space', '#sea'];
+var randomButton = document.getElementById('rando');
 
 var locationBurial = 'learn.html';
 
@@ -11,31 +12,13 @@ function assignValue(){
     if (burial === randoResults[i]) {
       console.log('burial',burial);
       window.location = locationBurial + elementArray[i];
-    
     }
   }
 }
 
-// document.getElementById('rando').onclick = function() {
-//   event.preventDefault();
-//   
-  
-
-// };
-
-var randomButton = document.getElementById('rando');
-
-randomButton.addEventListener('click', assignValue);
-
-// var burial;
 function randomizer() {
   var randomNumber = Math.floor(Math.random() * (randoResults.length - base)+ base);
-  console.log(randomNumber);
   return randoResults[randomNumber];
-  
-  //window.location = rando needs to connected to learn.html
 }
 
-
-//function randomHandler(event){
-//
+randomButton.addEventListener('click', assignValue);
